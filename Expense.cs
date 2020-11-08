811a9257ca987400ade302ebe9596865b6bda56e
Mirosław Ściebura <mirek.sciebura@gmail.com>
2020-11-08 19:13:17 +0100

    Maybe ended work on Expenses.

--- Expense.cs
+++ Expense.cs
@@ -6,13 +6,13 @@ namespace MyCar
 {
     class Expense
     {
-        public int ExpenseId { get; set; }
+        public int? ExpenseId { get; set; }
         public string ExpenseDate { get; set; }
         public string ExpensePlace { get; set; }
 
-        public int ExpenseMileage { get; set; }
+        public int? ExpenseMileage { get; set; }
 
-        public double ExpensePrice { get; set; }
+        public double? ExpensePrice { get; set; }
         public string ExpenseInfo { get; set; }
         public string ExpenseTypeInfo { get; set; }
     }
